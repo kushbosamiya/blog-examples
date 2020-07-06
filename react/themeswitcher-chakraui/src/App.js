eb44510c827af709a7a6ac372aae5b7710e89a80
Aman Mittal <amandeepmittal@live.com>
2020-07-06 18:09:14 +0530

    complete

--- react/themeswitcher-chakraui/src/App.js
+++ react/themeswitcher-chakraui/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  ThemeProvider,
+  theme,
+  ColorModeProvider,
+  CSSReset
+} from '@chakra-ui/core';
 
-function App() {
+import ThemeToggler from './components/ThemeToggler';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <ColorModeProvider>
+        <CSSReset />
+        <ThemeToggler />
+      </ColorModeProvider>
+    </ThemeProvider>
   );
 }
-
-export default App;
